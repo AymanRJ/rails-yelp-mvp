a9297a0e82f4ac386837d3fb24d14967ae32ee84
Luis Gabriel Dayeh <luisgabriel.dayeh@gmail.com>
2021-08-25 17:06:47 +0200

    need to fix css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Games::Pokemon.name,
+    address: Faker::Games::Pokemon.location,
+    category: Restaurant::CATEGORIES.sample
+  )
+  puts "#{restaurant.name} created"
+end
