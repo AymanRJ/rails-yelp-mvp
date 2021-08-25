fa6443ff8eb21ee714b1f98eb299451e68e79828
Luis Gabriel Dayeh <luisgabriel.dayeh@gmail.com>
2021-08-25 12:37:01 +0200

    add restaurant and review model

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = %w(chinese italian japanese french belgian)
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+
+  has_many :reviews, dependent: :destroy
+
+end
